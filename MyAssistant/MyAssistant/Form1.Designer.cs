248250a4eb2e4c75728fb86b2c5da253f36291b5
Onii Sasaki <82929255+Onkisu@users.noreply.github.com>
2022-06-10 14:34:08 +0700

    Add files via upload

---
+++ MyAssistant/MyAssistant/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace MyAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSTT = new System.Windows.Forms.TextBox();
+            this.txtTTS = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btn_listen = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(113, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Speech To Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(646, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Text To Speech";
+            // 
+            // txtSTT
+            // 
+            this.txtSTT.Location = new System.Drawing.Point(41, 142);
+            this.txtSTT.Multiline = true;
+            this.txtSTT.Name = "txtSTT";
+            this.txtSTT.Size = new System.Drawing.Size(319, 150);
+            this.txtSTT.TabIndex = 3;
+            // 
+            // txtTTS
+            // 
+            this.txtTTS.Location = new System.Drawing.Point(559, 142);
+            this.txtTTS.Multiline = true;
+            this.txtTTS.Name = "txtTTS";
+            this.txtTTS.Size = new System.Drawing.Size(319, 150);
+            this.txtTTS.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(445, 306);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // btn_listen
+            // 
+            this.btn_listen.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_listen.Location = new System.Drawing.Point(41, 306);
+            this.btn_listen.Name = "btn_listen";
+            this.btn_listen.Size = new System.Drawing.Size(112, 35);
+            this.btn_listen.TabIndex = 6;
+            this.btn_listen.Text = "Listen";
+            this.btn_listen.UseVisualStyleBackColor = true;
+            this.btn_listen.Click += new System.EventHandler(this.btn_listen_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_read.Location = new System.Drawing.Point(766, 306);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(112, 35);
+            this.btn_read.TabIndex = 7;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(369, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Volume";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 399);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_listen);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.txtTTS);
+            this.Controls.Add(this.txtSTT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSTT;
+        private System.Windows.Forms.TextBox txtTTS;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btn_listen;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MyAssistant/MyAssistant/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
+namespace MyAssistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_listen_Click(object sender, EventArgs e)
+        {
+            if (txtSTT.Text!="")
+            {
+                
+                SpeechSynthesizer ss = new SpeechSynthesizer();
+              
+                ss.SelectVoiceByHints((VoiceGender)VoiceAge.Child);
+                ss.Volume = trackBar1.Value;
+                ss.Speak(txtSTT.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Write Something first!");
+            }
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+            Grammar words = new DictationGrammar();
+            sr.LoadGrammar(words);
+            try
+            {
+                txtTTS.Text = "Listening Now...";
+                sr.SetInputToDefaultAudioDevice();
+                RecognitionResult result = sr.Recognize();
+                txtTTS.Clear();
+                txtTTS.Text = result.Text;
+            }
+            catch(Exception ex)
+            {
+                txtTTS.Text = "";
+                MessageBox.Show("Mic Not Found!");
+            }
+        }
+    }
+}
